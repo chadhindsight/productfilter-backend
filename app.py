5ee65cb2285944c97aadb26b42e89729e45f8fdb
chadhindsight <chadrickJ8@gmail.com>
2025-04-09 13:35:56 -0400

    Remove old server setup

--- app.py
+++ app.py
@@ -1,20 +1,3 @@
-# from flask import Flask
-# from flask_cors import CORS
-# from backend.routes import api
-
-# app = Flask(__name__)
-# CORS(app) 
-
-# app.register_blueprint(api, url_prefix='/api')  # Register blueprint with URL prefix
-
-# # Test route for validation
-# @app.route('/test')
-# def test():
-#     return "Test route is working!"
-
-# if __name__ == '__main__':
-#     app.run(debug=True)
-
 from flask import Flask
 from flask_cors import CORS
 from backend.routes import api
