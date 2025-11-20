631ff39eb3f3f666585fcfade50130a740b0390a
chadhindsight <chadrickJ8@gmail.com>
2025-11-20 14:58:50 -0500

    Add message

--- backend/routes.py
+++ backend/routes.py
@@ -5,7 +5,7 @@ api = Blueprint('api', __name__)
 
 @api.route('/api/products', methods=['GET'])
 def get_products():
-    print("GET /api/products was hit")  # log
+    print("GET /api/productsendpoint was hit")  
     return jsonify(products)
 
 @api.route('/test', methods=['GET'])
