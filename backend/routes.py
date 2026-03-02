91fec7d80d9f03f62e377df9be2866c5b3e18446
chadhindsight <chadrickJ8@gmail.com>
2026-03-02 12:38:20 -0500

    change the message printed on get_products()

--- backend/routes.py
+++ backend/routes.py
@@ -5,7 +5,7 @@ api = Blueprint('api', __name__)
 
 @api.route('/api/products', methods=['GET'])
 def get_products():
-    print("GET /api/productsendpoint was hit")  
+    print("GET /api/products was hit")  
     return jsonify(products)
 
 @api.route('/test', methods=['GET'])
